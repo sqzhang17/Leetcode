9d9432869a571ce6ae27f45370511439a602f97f
sqzhang17 <sqzhang17@gmail.com>
2015-07-19 19:12:02 -0700

    pass oj recursive call

---
+++ LowestCommonAncestorOfBST/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(!(p!=null && q!=null)) return null;
+        
+        if(root==null) return null;
+        
+        if(p.val>root.val && q.val>root.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }else if(p.val<root.val && q.val<root.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        
+        return root;
+        
+    }
+}
