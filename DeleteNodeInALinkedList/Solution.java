e35780ddf35980c48c9627a48efe02e7ad071838
sqzhang17 <sqzhang17@gmail.com>
2015-07-21 21:15:34 -0700

    time: O(1), space: O(1)

---
+++ DeleteNodeInALinkedList/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public void deleteNode(ListNode node) {
+        if(node==null) return; 
+        if(node.next==null) {node=null;}
+
+        node.val = node.next.val;
+        node.next = node.next.next; 
+    }
+}
