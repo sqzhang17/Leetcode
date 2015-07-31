d2d671cf9a1bc1f1d95b28293d2e4e524bdd31e8
Shiqi Zhang <ShiqiZhang@Shiqis-MacBook-Pro.local>
2015-07-30 20:14:47 -0700

    time:O(n^2) space: O(1)

---
+++ CountAndSay/Solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public String countAndSay(int n) {
+        if(n==0) return "";
+        
+        String prev = "1";
+        
+        for(int i=1; i<n; i++){
+            String res = new String();
+            int count = 1;
+            char cache =prev.charAt(0);
+            
+            for(int j=1; j<prev.length(); j++){
+                if(prev.charAt(j)==cache){
+                    count++;
+                }else{
+                    res +=count+""+cache;
+                    cache=prev.charAt(j);
+                    count=1;
+                }
+            }
+            
+            res +=count+""+prev.charAt(prev.length()-1);
+            
+            prev =res;
+        }
+        
+        return prev;
+    }
+}
