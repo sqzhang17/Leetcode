146084f33d2eb750b38d4b2591004fe6d85dcd8c
sqzhang17 <sqzhang17@gmail.com>
2015-09-08 16:44:49 -0700

    adding oj solution, time=O(n), space=O(1)

--- MaximumProductSubarray/Solution.java
+++ MaximumProductSubarray/Solution.java
@@ -2,6 +2,7 @@ public class Solution {
 	public static void main(String[] args){
 		Solution s = new Solution();
 		s.test1();
+		s.test2();
 	}
 
 	//tle solution
@@ -22,8 +23,35 @@ public class Solution {
         return max;
     }
 
+    public int maxProduct_v2(int[] nums){
+    	if(nums==null || nums.length==0) return 0;
+
+    	int maxUntilCurr = nums[0];
+    	int minUntilCurr = nums[0];
+    	int maxCurr=0;
+    	int minCurr=0;
+    	int maxSoFar=nums[0];
+
+    	for(int i=1;i<nums.length;i++){
+    		maxCurr = Math.max(Math.max(maxUntilCurr*nums[i], minUntilCurr*nums[i]), nums[i]);
+    		minCurr = Math.min(Math.min(minUntilCurr*nums[i], maxUntilCurr*nums[i]), nums[i]);
+
+    		if(maxCurr>maxSoFar) maxSoFar=maxCurr;
+
+    		maxUntilCurr=maxCurr;
+    		minUntilCurr=minCurr;
+    	}
+
+    	return maxSoFar;
+    }
+
     public void test1(){
     	int[] a = {2,3,-2,4};
     	System.out.println(maxProduct(a));
+    }
+
+    public void test2(){
+    	int[] a = {2,3,-2,4};
+    	System.out.println(maxProduct_v2(a));
     }
 }
