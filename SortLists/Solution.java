f13442ad42ef6653c414026badfca6d4fc8fd172
sqzhang17 <sqzhang17@gmail.com>
2015-09-09 15:21:45 -0700

    ac, recursive method, time=O(NlogN), space=O(1)

---
+++ SortLists/Solution.java
@@ -0,0 +1,70 @@
+
+ //Definition for singly-linked list.
+ class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class Solution {
+    public ListNode sortList(ListNode head) {
+     if(head==null || head.next==null) return head;
+
+     ListNode f = head;
+     ListNode s = head; 
+
+     while(f.next!=null && f.next.next!=null){
+     	f=f.next.next;
+     	s=s.next;
+     }   
+
+     ListNode shead = s.next; 
+
+     s.next=null;
+
+     head = sortList(head);
+     shead = sortList(shead);
+
+     return mergeTwoLists(head, shead);
+
+
+    }
+
+    private ListNode mergeTwoLists(ListNode ln1, ListNode ln2){
+    	if(ln1==null) return ln2;
+    	if(ln2==null) return ln1;
+
+    	ListNode newHeadPrev = new ListNode(0);
+    	newHeadPrev.next = ln1.val>ln2.val? ln2:ln1;
+
+    	ListNode pointer = newHeadPrev;
+    	while(ln1!=null && ln2!=null){
+    		if(ln1==null){
+    			pointer.next=ln2;
+    			return newHeadPrev.next;
+    		}
+
+    		if(ln2==null){
+    			pointer.next=ln1;
+    			return newHeadPrev.next;
+    		}
+
+    		if(ln1.val>ln2.val){
+    			pointer.next=ln2;
+    			ln2=ln2.next;
+    			pointer=ln2;
+    		}else{
+    			pointer.next=ln1;
+    			ln1=ln1.next;
+    			pointer=ln1;
+    		}
+    	}
+    	return newHeadPrev.next;
+    }
+
+    public static void main(String[] args){
+    	Solution s = new Solution();
+    	s.test1();
+    }
+    
+}
