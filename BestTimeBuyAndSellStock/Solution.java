3c72127ab8a5fa09d78be0b627f5adc275b45091
sqzhang17 <sqzhang17@gmail.com>
2015-09-11 11:16:35 -0700

    time=O(n)

--- BestTimeBuyAndSellStock/Solution.java
+++ BestTimeBuyAndSellStock/Solution.java
@@ -1,8 +1,12 @@
+import java.util.*;
 public class Solution {
 	public static void main(String[] args){
 		Solution s = new Solution();
 		s.test1();
+		s.test2();
 	}
+
+	//tle solution
     public int maxProfit(int[] prices) {
         if(prices ==null || prices.length<=1)return 0;
 
@@ -19,9 +23,27 @@ public class Solution {
         return max;
     }
 
+    public int maxProfit_v2(int[] prices){
+    	if(prices ==null || prices.length<=1)return 0;
+
+    	int min = prices[0];
+    	int profit = 0;
+
+    	for(int i=0; i<prices.length;i++){
+    		min = Math.min(prices[i], min);
+    		profit= Math.max(profit, prices[i]-min);
+    	}
+
+    	return profit;
+    }
     public void test1(){
-    	int[] a={1,2,1,4,5};
+    	int[] a={1,2,9,4,5};
 
     	System.out.println(maxProfit(a));
+    }
+    public void test2(){
+    	int[] a={1,2,9,4,5};
+
+    	System.out.println(maxProfit_v2(a));
     }
 }
