d6dd36b52e3413e8e66547cfea291c2154d24739
sqzhang17 <sqzhang17@gmail.com>
2015-09-12 18:19:54 -0700

    delete the case for left bound == right bound, still ac

--- ConvertSortedArrayToBinarySearchTree/Solution.java
+++ ConvertSortedArrayToBinarySearchTree/Solution.java
@@ -8,16 +8,16 @@ public class Solution {
     private TreeNode helper(int[] a, int l, int r){
     	if(l<0 || r>=a.length || l>r) return null;
 
-    	if(l==r){
-    		return new TreeNode(a[l]);
-    	}else{
+    	//if(l==r){
+    	//	return new TreeNode(a[l]);
+    	//}else{
     		int m = (l+r)/2;
 
     		TreeNode temp = new TreeNode(a[m]);
     		temp.left=helper(a,l,m-1);
     		temp.right=helper(a,m+1,r);
     		return temp;
-    	}
+    	//}
     }
 }
 
