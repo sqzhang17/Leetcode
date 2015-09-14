e45f91912098dcc446ef3b83a735beb24135b5ed
sqzhang17 <sqzhang17@gmail.com>
2015-09-14 11:33:29 -0700

    adding comments

--- SwapNodesInPairs/Solution.java
+++ SwapNodesInPairs/Solution.java
@@ -1,10 +1,20 @@
+/*
+Key point:
+	1.	use dummy head to track the new head of swaped list
+	2.	use pointer to track the position to insert
+	3.	visit each listnode and swap in pair 
+*/
+
 public class Solution {
     public ListNode swapPairs(ListNode head) {
+        //dummy node before the head
         ListNode dummyHead = new ListNode(-1);
         dummyHead.next=head;
 
+        //keep track of the last position to insert
         ListNode pointer = dummyHead;
         
+        //loop every two node and swap 
         while(pointer.next!=null && pointer.next.next!=null){
         	ListNode temp = pointer.next;
 
