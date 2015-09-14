2d0118a5418b13995dd4d63906102619af13ab2e
sqzhang17 <sqzhang17@gmail.com>
2015-09-14 11:02:46 -0700

    ac solution, faster, time=O(m*n), space=O(1)

--- MinimumPathSum/Solution.java
+++ MinimumPathSum/Solution.java
@@ -4,7 +4,7 @@ key point:
 		since there is only one direction to reach that element.
 	2.	traverse each elemetn besides first row and first column,
 		update that element by comparing the up and left element
-		
+
 @author: Shiqi Zhang
 */
 
@@ -42,4 +42,29 @@ public class Solution {
 
     	System.out.println(minPathSum(a));
     }
+/*
+Key point:
+	1.	DP within the loop
+	2.	consider all the case within the loop
+*/
+    public int minPathSum_v2(int[][] grid) {
+		int m = grid.length;// row
+	    int n = grid[0].length; // column
+	    for (int i = 0; i < m; i++) {
+	        for (int j = 0; j < n; j++) {
+	            if (i == 0 && j != 0) {
+	                grid[i][j] = grid[i][j] + grid[i][j - 1];
+	            } else if (i != 0 && j == 0) {
+	                grid[i][j] = grid[i][j] + grid[i - 1][j];
+	            } else if (i == 0 && j == 0) {
+	                grid[i][j] = grid[i][j];
+	            } else {
+	                grid[i][j] = Math.min(grid[i][j - 1], grid[i - 1][j])
+	                        + grid[i][j];
+	            }
+	        }
+	    }
+
+	    return grid[m - 1][n - 1];
+	}
 }
