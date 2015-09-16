9f8328259ae74998ed58ef36af90b33ae07bf409
sqzhang17 <sqzhang17@gmail.com>
2015-09-16 11:23:47 -0700

    adding comment

--- RemoveDuplicatesFromSortedArrayII/Solution.java
+++ RemoveDuplicatesFromSortedArrayII/Solution.java
@@ -6,6 +6,8 @@ public class Solution {
         int ptr=2;
         for(int i=2; i<nums.length; i++){
         	if(nums[i]>nums[ptr-2]){
+        	//above if clause can be changed into
+        	//if(!(nums[i]==nums[ptr-2] && nums[i]==nums[ptr-1]))
         		nums[ptr]=nums[i];
         		ptr++;
         	}
