32fa4eecba109d06def20dac983a080588ea6004
sqzhang17 <sqzhang17@gmail.com>
2015-09-16 10:28:42 -0700

    adding faster solution, time=O(logN) worst=O(N)

--- SearchInRotatedSortedArrayII/Solution.java
+++ SearchInRotatedSortedArrayII/Solution.java
@@ -1,30 +1,66 @@
 public class Solution {
-    public boolean search(int[] nums, int target) {
-        if(nums==null || nums.length<1) return false;
+    public boolean search(int[] A, int target) {
+        int l = 0;
+        int r = A.length - 1;
+        while(l <= r){
+            int mid = (l + r) / 2;
+            if(A[mid] == target)
+                return true;
+            else if(A[mid] < A[l]){
+                if(target > A[r])
+                    r = mid - 1;
+                else if(target < A[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }else if(A[mid] > A[l]){
+                if(target < A[mid] && target >= A[l])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }else{ // A[mid] == A[l]
+                if(A[mid] != A[r])
+                    l = mid + 1;
+                else{
+                    boolean flag = true;
+                    for(int k = 1; mid - k >= l && mid + k <= r; k++){
+                        if(A[mid] != A[mid - k]){
+                            r = mid - k;
+                            flag = false;
+                            break;
+                        }else if(A[mid] != A[mid + k]){
+                            l = mid + k;
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(flag)
+                        return false;
+                }
+            }
+        }
+        return false;
+    }
 
-        int left = 0;
-        int right = nums.length-1;
 
-        while(left<=right){
-        	int mid = (left+right)/2;
+    public boolean search_v2(int[] A, int target){
+        int lo =0, hi = A.length-1;
+        int mid = 0;
+        while(lo<hi){
+              mid=(lo+hi)/2;
+              if(A[mid]==target) return true;
+              if(A[mid]>A[hi]){
+                  if(A[mid]>target && A[lo] <= target) hi = mid;
+                  else lo = mid + 1;
+              }else if(A[mid] < A[hi]){
+                  if(A[mid]<target && A[hi] >= target) lo = mid + 1;
+                  else hi = mid;
+              }else{
+                  hi--;
+              }
 
-        	if (nums[mid] == target) // case 0
-            return true;
-	        // finally start == mid == end, if case 0, return true, else end the loop
-	        else if (nums[left] == nums[mid])
-	            left=mid+1;
-	        else if (nums[right] == nums[mid])
-	            right=mid-1;
-	        else if (nums[left] <= target && target <= nums[mid]) // case 1
-	            right = mid;
-	        else if (nums[mid] < target && target <= nums[right]) // case 2
-	            left = mid + 1;
-	        else if (nums[left] > nums[mid]) // case 2 is false, so target in this range
-	            right = mid;
-	        else   // case A[mid] > A[right] and case 1 is false, similar to above
-	            left = mid + 1;
-	    }
-	        return false;
+        }
+        return A[lo] == target ? true : false;
     }
 
     public static void main(String[] args){
