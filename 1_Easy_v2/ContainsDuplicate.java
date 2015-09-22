ba33baf26f954f4959425b11b1587c7188a6206b
sqzhang17 <sqzhang17@gmail.com>
2015-09-21 18:54:17 -0700

    add ac solution, time=O(N), space=O(1)

--- 1_Easy_v2/ContainsDuplicate.java
+++ 1_Easy_v2/ContainsDuplicate.java
@@ -14,5 +14,14 @@ public class ContainsDuplicate {
         return false;
     }
 
-    
+    public boolean containsDuplicate_v2(int[] nums) {
+        if(nums==null || nums.length<2) return true;
+        Arrays.sort(nums);
+
+        for(int i=1; i<nums.length;i++){
+        	if(nums[i]==nums[i-1]) return true;
+        }
+
+        return false;
+    }
 }
