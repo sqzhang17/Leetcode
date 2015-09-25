5c7ee7874a3b856f387e14f40ca2bf2e3e03efe0
sqzhang17 <sqzhang17@gmail.com>
2015-09-25 11:22:30 -0700

    adding comment

--- InsertionSortList/Solution.java
+++ InsertionSortList/Solution.java
@@ -5,18 +5,20 @@ public class Solution {
         ListNode ptr = dummy;
         ListNode curr = head;
         while(curr!=null){
+        	//find the right spot to insert
         	while(ptr.next !=null && ptr.next.val<curr.val){
         		ptr=ptr.next;
         	}
 
+        	//insert the curr node
         	ListNode temp1 = curr.next;
         	ListNode temp2 = ptr.next;
         	ptr.next=curr;
         	curr.next=temp2;
         	curr=temp1;
         	
+        	//update ptr node
         	ptr=dummy;
-
         }
 
         return dummy.next;
--- SearchForRange/Solution.java
+++ SearchForRange/Solution.java
@@ -1,8 +1,12 @@
 public class Solution {
 	public int[] searchRange(int[] nums, int target) {
 	    int[] result = new int[2];
+	    result[1] = -1;
 	    result[0] = findFirst(nums, target);
-	    result[1] = findLast(nums, target);
+	    if(result[0]==-1)
+	   		return result;
+
+	   	result[1] = findLast(nums, target);
 	    return result;
 	}
 
@@ -20,6 +24,22 @@ public class Solution {
 	        if(nums[mid] == target) idx = mid;
 	    }
 	    return idx;
+	    /*
+		int idx = -1;
+	    int start = 0;
+	    int end = nums.length - 1;
+	    while(start <= end){
+	        int mid = (start + end) / 2;
+	        if(nums[mid] > target){
+	            end = mid - 1;
+	        }else if(nums[mid] < target){
+	            start = mid + 1;
+	        }else{
+	        	idx = mid;
+	        }
+	    }
+	    return idx;
+	    */
 	}
 
 	private int findLast(int[] nums, int target){
