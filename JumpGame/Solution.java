dbd3f540a75e275b358fa0375d8a2890318cdfeb
sqzhang17 <sqzhang17@gmail.com>
2015-09-25 16:20:11 -0700

    ac solution, time=O(n)

---
+++ JumpGame/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums==null || nums.length==0) return false;
+        int reachable =0;
+        for(int i=0; i<nums.length; i++){
+            if(i>reachable){
+                return false;
+            }
+            reachable = Math.max(reachable, i+nums[i]);
+        }
+        
+        return true;
+    }
+}
