422a2862509a02a7ce2d8405cb6ee1696555c94d
sqzhang17 <sqzhang17@gmail.com>
2015-09-25 19:30:10 -0700

    ac solution, time=O(logN)

---
+++ Pow(x_n)/Solution.java
@@ -0,0 +1,16 @@
+ public class Solution {
+        public double myPow(double x, int m) {
+            if(m==0) return 1;
+
+            double temp=myPow(x,m/2);
+            if(m%2==0) {
+                return temp*temp;
+            }else{
+                if(m > 0)
+                    return x*temp*temp;
+                else
+                    return (temp*temp)*(1.0/x);
+            }
+
+    }
+}
