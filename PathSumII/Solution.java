38877bba7d4f245d19ab9f230bc170be77031c54
sqzhang17 <sqzhang17@gmail.com>
2015-09-27 14:33:49 -0700

    adding comment

--- PathSumII/Solution.java
+++ PathSumII/Solution.java
@@ -9,25 +9,30 @@ public class Solution {
     private void pathSumCheck(TreeNode root, int target,int sumTillParent, List<Integer> listTillParent){
     	if(root==null) return;
 
+    	//meet leaf node, and sum ==target
     	if(root.left==null && root.right==null && root.val+sumTillParent==target){
     	    listTillParent.add(root.val);
     		res.add(listTillParent);
     	}
 
+    	//not reach the target
     	listTillParent.add(root.val);
 
-
+    	//reaching left node
     	if(root.left!=null){
     		pathSumCheck(root.left, target, root.val+sumTillParent, new ArrayList<Integer>(listTillParent));
     	}
 
+    	//reaching right node
     	if(root.right!=null){
     		pathSumCheck(root.right, target, root.val+sumTillParent, new ArrayList<Integer>(listTillParent));
     	}
 
+    	//delete the last for going up to parent node 
     	listTillParent.remove(listTillParent.size()-1);
 
-    	return;
+
+    	//return;
     }
 
 }
