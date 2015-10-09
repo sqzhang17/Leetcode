763271a670c05335dd7dbc9988f2c595b4fe6038
sqzhang17 <sqzhang17@gmail.com>
2015-10-08 18:03:14 -0700

    ac solution, using HashMap

--- GroupAnagrams/Solution.java
+++ GroupAnagrams/Solution.java
@@ -1,5 +1,5 @@
 public class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
+    public List<List<String>> groupAnagrams_tle(String[] strs) {
         List<List<String>> res = new ArrayList<List<String>>();
 
         if(strs==null || strs.length==0) return res; 
@@ -38,5 +38,31 @@ public class Solution {
         for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
         for (int i : alphabet) if (i != 0) return false;
         return true;
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        List<List<String>> res = new ArrayList<List<String>>();
+
+        if(strs==null || strs.length==0) return res;
+
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+
+        Arrays.sort(strs);
+
+        for(String s: strs){
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+
+            String newString = new String(charArray);
+
+            if(!map.containsKey(newString)){
+                map.put(newString, new ArrayList<String>());
+            }
+
+            map.get(newString).add(s);
+        }
+
+        return new ArrayList<List<String>>(map.values());
     }
 }
