6589519becc099d759f92a148864c119432f3c44
sqzhang17 <sqzhang17@gmail.com>
2015-10-09 12:25:24 -0700

    ac solution with DP

--- WordBreak/Solution.java
+++ WordBreak/Solution.java
@@ -15,4 +15,24 @@ public class Solution {
 
         return false;
     }
+
+    
+    //It is a DP solution, 
+    //the array breakable[i] stores whether the substring s.substring(0,i) is breakable or not. 
+    //The DP equation is as follows: breakable[i+1] |= breakable[j]&&dict.contains(s.substring(j,i+1)), 
+    //j>=0 && j<i+1
+    public boolean wordBreak(String s, Set<String> dict) {
+        boolean [] breakable = new boolean[s.length()+1];
+        breakable[0] = true;
+
+        for(int i=1;i<=s.length();i++){
+            for(int j=0;j<i;j++){
+                if(breakable[j]&&dict.contains(s.substring(j,i))){
+                    breakable[i] = true;
+                    break;
+                }
+            }
+        }
+        return breakable[s.length()];
+    }
 }
