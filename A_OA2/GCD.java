9736a3e5935f0988965ad5a52ef8a55eb39dadff
sqzhang17 <sqzhang17@gmail.com>
2015-10-15 16:29:19 -0700

    gcd of an array

--- A_OA2/GCD.java
+++ A_OA2/GCD.java
@@ -3,4 +3,12 @@ public class GCD{
 	   if (b==0) return a;
 	   return GCD(b,a%b);
 	}
+
+	public int gcdArray(int[] numbers){
+		int result = numbers[0];
+		for(int i = 1; i < numbers.length; i++){
+		    result = gcd(result, numbers[i]);
+		}
+		return result;
+	}
 }
