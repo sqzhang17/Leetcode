e5f066eb183bef81f9f18caa9882fd454e09c7c0
sqzhang17 <sqzhang17@gmail.com>
2015-10-17 19:20:24 -0700

    ac solution, with HashSet

---
+++ 1_Easy_v2/ContainsDuplicate_v2.java
@@ -0,0 +1,14 @@
+public class ContainsDuplicate_v2 {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums==null || nums.length==0) return false;
+        
+        Arrays.sort(nums);
+        
+        for(int i=1; i<nums.length; i++){
+            if(nums[i]==nums[i-1]) 
+                return true;
+        }
+        
+        return false;
+    }
+}
